509bcb1ce8ce54663646cfad59985073ed8650eb
kennansalisbury <kennansalisbury@gmail.com>
2020-07-23 18:23:25 -0700

    stubbed iss page

--- src/content/pages/ISS.js
+++ src/content/pages/ISS.js
@@ -4,13 +4,19 @@ import { Redirect } from 'react-router-dom'
 
 const ISS = props => {
 
+    // ***** TO DO: if props.user, fetch data from iss api on render (useEffect) and generate map
+
     //if no user is logged in, redirect to login
     if(!props.user) {
         return <Redirect to="/login" />
     }
 
     return (
-        <div>ISS Location</div>
+        <div className="iss">
+            
+            <h1>ISS Location</h1>
+        
+        </div>
     )
 }
 
