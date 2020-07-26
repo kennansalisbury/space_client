842f751935d180fbf8a6c48a90db208078f8dc18
kennansalisbury <kennansalisbury@gmail.com>
2020-07-25 21:17:56 -0700

    initial set up to fetch astronaut twitter info

--- src/content/pages/Astronauts/components/TableRow.js
+++ src/content/pages/Astronauts/components/TableRow.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { catchError } from '../../../../helpers/Helpers'
 
 const TableRow = props => {
 
     let astronaut = props.astronaut
+    const [twitter, setTwitter] = useState('')
+
+    useEffect(() => {
+        //post search to backend to query api
+        let token = localStorage.getItem('userToken')
+        fetch('http://localhost:3000/twitter/user', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
+    }, [])
 
     return (
         <div className="table__row">
             <p className="left_text">{astronaut.name}</p>
             <p>{astronaut.craft}</p>
-            <p><i className="fab fa-twitter-square"></i></p>
+            {twitter ? <p><i className="fab fa-twitter-square"></i></p> : '' }
         </div>
     )
 }
--- src/content/pages/Profile/Profile.js
+++ src/content/pages/Profile/Profile.js
@@ -15,8 +15,8 @@ const Profile = props => {
     return (
         <div className="profile">
             <div>
-                <h1>Welcome {props.user.screen_name}!</h1>
-                {/* image */}
+                <h1>Welcome {props.user.name}!</h1>
+                <img src={props.user.profile_image_url} alt="profile_photo" />
                 <p>What would you like to do first?</p>
             </div>
             
